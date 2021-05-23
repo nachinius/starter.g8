5efd14b7337b12e853baf56f018de4ccd51a13a4
nachinius <ignacio.peixoto@gmail.com>
2021-05-23 16:25:10 +0200

    Update Dependencies.scala

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -5,18 +5,22 @@ object Dependencies {
   object Versions {
 
     val cats          = "2.1.1"
-    val catsEffect    = "2.1.2"
+    val catsEffect    = "2.5.1"
     val catsMeowMtl   = "0.4.0"
     val catsRetry     = "1.1.0"
     val circe         = "0.13.0"
     val ciris         = "1.0.4"
-    val fs2           = "2.3.0"
+    val fs2           = "2.5.3"
 
     // http
     val http4s        = "0.21.3"
     val http4sJwtAuth = "0.0.4"
+    val sttp          = "3.3.4"
 
+    // config
+    val pureconfig    = "0.15.0"
 
+    // misc
     val log4cats      = "1.0.1"
     val newtype       = "0.4.3"
     val refined       = "0.9.13"
@@ -39,9 +43,10 @@ object Dependencies {
     val kindProjector    = "0.11.3"
     val logback          = "1.2.3"
 
+    //test
     val scalaCheck    = "1.14.3"
     val scalaTest     = "3.2.0"
-    val munit         = "0.7.22"
+    val munit         = "0.7.26"
   }
 
   object Libraries {
@@ -118,6 +123,10 @@ object Dependencies {
     // Test
     val munit         = "org.scalameta" %% "munit" % Versions.munit % Test
 
+    val sttp = "com.softwaremill.sttp.client3" %% "core" % Versions.sttp
+    val sttpAsync = "com.softwaremill.sttp.client3" %% "async-http-client-backend-fs2-ce2" % Versions.sttp
+
+    val pureConfig = "com.github.pureconfig" %% "pureconfig" % Versions.pureconfig
   }
 
   object Resolvers {

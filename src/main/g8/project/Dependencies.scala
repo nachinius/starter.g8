7e52e01b1fd8061d5777086fef885735fa45b95f
nachinius <ignacio.peixoto@gmail.com>
2023-08-21 17:54:25 +0200

    Update Dependencies.scala

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -2,51 +2,54 @@ import sbt._
 
 object Dependencies {
 
-  object Versions {
+   object Versions {
 
-    val cats          = "2.1.1"
-    val catsEffect    = "2.5.1"
-    val catsMeowMtl   = "0.4.0"
-    val catsRetry     = "1.1.0"
-    val circe         = "0.13.0"
-    val ciris         = "1.0.4"
-    val fs2           = "2.5.3"
+    // functional programming
+    val cats          = "2.10.0"
+    val catsEffect    = "3.5.1"
+    val catsMeowMtl   = "0.5.0"
+    val catsRetry     = "3.1.0"
+    val circe         = "0.14.5"
+    val ciris         = "3.2.0"
+    val fs2           = "3.8.0"
 
     // http
-    val http4s        = "0.21.3"
+    val http4s        = "0.23.3"
     val http4sJwtAuth = "0.0.4"
-    val sttp          = "3.3.4"
+    val sttp          = "3.3.15"
 
     // config
-    val pureconfig    = "0.15.0"
+    val pureconfig    = "0.16.0"
 
-    // misc
-    val log4cats      = "1.0.1"
-    val newtype       = "0.4.3"
-    val refined       = "0.9.13"
-    val redis4cats    = "0.9.6"
-    val squants       = "1.6.0"
-    val monocle       = "2.0.0"
-    val tapir         = "0.15.4"
+    // logging
+    val log4cats      = "2.1.1"
+
+    // Mis
+    val newtype       = "0.4.4"
+    val refined       = "0.9.27"
+    val redis4cats    = "0.11.0"
+    val squants       = "1.8.3"
+    val monocle       = "3.0.0"
+    val tapir         = "0.18.3"
 
     // db
-    val skunk         = "0.0.8"
-    val quill         = "3.5.2"
-    val doobie        = "0.8.8"
+    val skunk         = "0.2.2"
+    val quill         = "3.9.0"
+    val doobie        = "1.0.0"
 
 
     // generic programming
-    val shapless      = "2.3.3"
+    val shapeless      = "2.3.7"
 
     // compiler plugins
     val betterMonadicFor = "0.3.1"
-    val kindProjector    = "0.11.3"
-    val logback          = "1.2.3"
+    val kindProjector    = "0.13.2"
+    val logback          = "1.2.6"
 
     //test
-    val scalaCheck    = "1.14.3"
-    val scalaTest     = "3.2.0"
-    val munit         = "0.7.26"
+    val scalaCheck    = "1.15.4"
+    val scalaTest     = "3.2.10"
+    val munit         = "0.7.29"
   }
 
   object Libraries {
